27bc77bdfdc191bd7127d701e7f3242e7d4f748f
Michele Casadei <127590217+MicheleCasadei706@users.noreply.github.com>
2023-05-10 14:57:27 +0200

    Add files via upload

---
+++ Michele.Casadei.3i.isogramma/Isogramma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+     var Alfabeto = new int [26];
+        foreach(var Lettera in word.ToLower())
+        {
+            if (Char.IsLetter(Lettera))
+            {
+                Alfabeto [Lettera - 'a']++;
+                if (Alfabeto[Lettera - 'a'] > 1)
+                    return false;
+            }
+        }
+        
+        return true;
+
+    }
+}
